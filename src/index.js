aa9d328ba4a658c71b9de2855195906cfc9e0a6e
ryantohhr <ryantohhongrui@hotmail.com>
2024-11-15 17:54:25 +0800

    Added basic HTML and CSS, removed html-loader from webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./style.css";
-import ToDo from "./todo-mod.js";
-import Project from "./projects-mod.js";
+import { ToDo } from "./todo-mod.js";
+import { Project } from "./projects-mod.js";
 
--- webpack.common.js
+++ webpack.common.js
@@ -19,10 +19,6 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
         ],
     },
 };
