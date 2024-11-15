54a45d888d73fefa3a87a55a6fe504b54ae6b5b6
ryantohhr <ryantohhongrui@hotmail.com>
2024-11-15 13:25:21 +0800

    Implemented ToDo constructor and editToDo

---
+++ src/todo-class.js
@@ -0,0 +1,20 @@
+export class ToDo {
+    constructor(title, desc, dueDate, priority) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.checked = false;
+    }
+
+    editToDo(newTitle, newDesc, newDueDate, newPriority) {
+        this.title = newTitle;
+        this.desc = newDesc;
+        this.dueDate = newDueDate;
+        this.priority = newPriority;
+    }
+
+    checkToDo() {
+        
+    }
+}
