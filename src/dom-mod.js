b1782661338091d10de193cbeaa590482aa7e065
ryantohhr <ryantohhongrui@hotmail.com>
2024-11-16 21:01:47 +0800

    Moved renderModals to dom-mod.js

--- src/dom-mod.js
+++ src/dom-mod.js
@@ -46,3 +46,55 @@ export const DOMManipulator = (function() {
 
     return { displayToDo, displayProject };
 })();
+
+export const renderModals = (function() {
+    function renderProjectModal() {
+        const addProjectModal = document.querySelector(".add-project-modal");
+        const newProjectBtn = document.querySelector(".new-project");
+
+        newProjectBtn.addEventListener('click', () => {
+            addProjectModal.style.display = "grid";
+        })
+
+        const projectModalClose = document.querySelector(".project-modal-close");
+        projectModalClose.addEventListener('click', () => {
+            addProjectModal.style.display = "none";
+        })
+    }
+
+    function renderToDoModal() {
+        const addToDoModal = document.querySelector(".add-todo-modal");
+        const plusToDoBtn = document.querySelector(".add-button");
+
+        plusToDoBtn.addEventListener('click', () => {
+            addToDoModal.style.display = "grid";
+        })
+
+        const toDoModalClose = document.querySelector(".todo-modal-close");
+        toDoModalClose.addEventListener('click', () => {
+            addToDoModal.style.display = "none";
+        })
+
+        const lowPrioBtn = document.querySelector(".low-priority-button");
+        const medPrioBtn = document.querySelector(".medium-priority-button");
+        const highPrioBtn = document.querySelector(".high-priority-button");
+
+        lowPrioBtn.addEventListener('click', () => {
+            lowPrioBtn.classList.add("low-priority-button-active");
+            medPrioBtn.classList.remove("medium-priority-button-active");
+            highPrioBtn.classList.remove("high-priority-button-active");
+        });
+        medPrioBtn.addEventListener('click', () => {
+            lowPrioBtn.classList.remove("low-priority-button-active");
+            medPrioBtn.classList.add("medium-priority-button-active");
+            highPrioBtn.classList.remove("high-priority-button-active");
+        })
+        highPrioBtn.addEventListener('click', () => {
+            lowPrioBtn.classList.remove("low-priority-button-active");
+            medPrioBtn.classList.remove("medium-priority-button-active");
+            highPrioBtn.classList.add("high-priority-button-active");
+        })
+    }
+
+    return { renderProjectModal, renderToDoModal };
+})();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { ToDo } from "./todo-mod.js";
 import { Project } from "./projects-mod.js";
-import { DOMManipulator } from "./dom-mod.js";
+import { DOMManipulator, renderModals } from "./dom-mod.js";
 
 const main = (function() {
     function addProject(title) {
@@ -12,58 +12,7 @@ const main = (function() {
     return { addProject };
 })();
 
-const renderModals = (function() {
-    function renderProjectModal() {
-        const addProjectModal = document.querySelector(".add-project-modal");
-        const newProjectBtn = document.querySelector(".new-project");
-
-        newProjectBtn.addEventListener('click', () => {
-            addProjectModal.style.display = "grid";
-        })
-
-        const projectModalClose = document.querySelector(".project-modal-close");
-        projectModalClose.addEventListener('click', () => {
-            addProjectModal.style.display = "none";
-        })
-    }
-
-    function renderToDoModal() {
-        const addToDoModal = document.querySelector(".add-todo-modal");
-        const plusToDoBtn = document.querySelector(".add-button");
-
-        plusToDoBtn.addEventListener('click', () => {
-            addToDoModal.style.display = "grid";
-        })
-
-        const toDoModalClose = document.querySelector(".todo-modal-close");
-        toDoModalClose.addEventListener('click', () => {
-            addToDoModal.style.display = "none";
-        })
-
-        const lowPrioBtn = document.querySelector(".low-priority-button");
-        const medPrioBtn = document.querySelector(".medium-priority-button");
-        const highPrioBtn = document.querySelector(".high-priority-button");
-
-        lowPrioBtn.addEventListener('click', () => {
-            lowPrioBtn.classList.add("low-priority-button-active");
-            medPrioBtn.classList.remove("medium-priority-button-active");
-            highPrioBtn.classList.remove("high-priority-button-active");
-        });
-        medPrioBtn.addEventListener('click', () => {
-            lowPrioBtn.classList.remove("low-priority-button-active");
-            medPrioBtn.classList.add("medium-priority-button-active");
-            highPrioBtn.classList.remove("high-priority-button-active");
-        })
-        highPrioBtn.addEventListener('click', () => {
-            lowPrioBtn.classList.remove("low-priority-button-active");
-            medPrioBtn.classList.remove("medium-priority-button-active");
-            highPrioBtn.classList.add("high-priority-button-active");
-        })
-    }
-
-    renderProjectModal();
-    renderToDoModal();
-})();
 
 
-
+renderModals.renderProjectModal();
+renderModals.renderToDoModal();
